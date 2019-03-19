3c5b361a40431d2540ea259090cabebb4987ce25
qyd <saviorbruce@gmail.com>
2019-03-19 12:44:24 -0700

    implement add molecule and submit job functions

--- auto_bonds_fitting/auto_bonds_angles.py
+++ auto_bonds_fitting/auto_bonds_angles.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 import os
-from forcebalance.molecule import Molecule
+import copy
+from forcebalance.molecule import Molecule, Elements, bohr2ang
 import qcportal as ptl
 
 class FBTargetBuilder:
@@ -16,11 +17,17 @@ class FBTargetBuilder:
 
     def fb_molecule_to_qc_molecule(self, fb_molecule):
         """ Convert an forcebalance.molecule.Molecule object to a qcportal.Molecule object"""
-        return 1
+        e_idxs = [Elements.index(i) for i in fb_molecule.elem]
+        coords = fb_molecule.xyzs[0]
+        moldata = [[ei] + coord.tolist() for ei, coord in zip(e_idxs, coords)]
+        return ptl.Molecule.from_data(moldata, dtype="numpy", units="angstrom")
 
-    def qc_molecule_to_fb_molecule(self, fb_molecule):
+    def qc_molecule_to_fb_molecule(self, qc_molecule):
         """ Convert an qcportal.Molecule object to a forcebalance.molecule.Molecule object"""
-        return 1
+        m = Molecule()
+        m.elem = [Elements[i] for i in qc_molecule.atomic_numbers]
+        m.xyzs = [qc_molecule.geometry * bohr2ang]
+        return m
 
     def master(self):
 
@@ -68,29 +75,94 @@ class FBTargetBuilder:
         # opt_schema = {}
         # jobId = self.client.add_procedure('optimization', opt_schema)
         # return jobId
-        return 1
+        options = {
+            "keywords": None,
+            "qc_spec": {
+                "driver": "gradient",
+                "method": "HF",
+                "basis": "sto-3g",
+                "keywords": None,
+                "program": "psi4"
+            },
+        }
+        r = self.client.add_procedure("optimization", "geometric", options, qc_mol)
+        assert len(r.ids) == 1
+        return r.ids[0]
 
     def submit_bond_streching_jobs(self):
-        bonds = self.m.bonds
-        job_ids = []
-        for bond in bonds:
-            # schema = {}
-            # perturb the length of each bond by -30%, -20%, -10%, -5%, 5% ...
-            # job_id = self.client.add_service('grid_optimization', schema)
-            # job_ids.append(job_id)
-            pass
-        return job_ids
+        grid_opt_option_template = {
+            "keywords": {
+                "preoptimization": True,
+                "scans": [{
+                    "type": "distance",
+                    "indices": None, # To be filled
+                    "steps": None, # To be filled
+                    "step_type": "relative"
+                }]
+            },
+            "optimization_spec": {
+                "program": "geometric",
+                "keywords": {
+                    "coordsys": "tric",
+                }
+            },
+            "qc_spec": {
+                "driver": "gradient",
+                "method": "HF",
+                "basis": "sto-3g",
+                "keywords": None,
+                "program": "psi4",
+            },
+            "initial_molecule": self.qc_mol,
+        }
+        all_job_options = []
+        # perturb the length of each bond by -30%, -20%, -10%, -5%, 5% ...
+        strech_steps = [-0.25, -0.2, -0.15, -0.1, -0.05, 0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3]
+        for bond in self.m.bonds:
+            job_option = copy.deepcopy(grid_opt_option_template)
+            job_option['keywords']['scans'][0]['indices'] = list(bond)
+            job_option['keywords']['scans'][0]['steps'] = strech_steps
+            all_job_options.append(job_option)
+        r = self.client.add_service(all_job_options)
+        return r.ids
 
     def submit_angle_bending_jobs(self):
+        grid_opt_option_template = {
+            "keywords": {
+                "preoptimization": True,
+                "scans": [{
+                    "type": "angle",
+                    "indices": None, # To be filled
+                    "steps": None, # To be filled
+                    "step_type": "absolute"
+                }]
+            },
+            "optimization_spec": {
+                "program": "geometric",
+                "keywords": {
+                    "coordsys": "tric",
+                }
+            },
+            "qc_spec": {
+                "driver": "gradient",
+                "method": "HF",
+                "basis": "sto-3g",
+                "keywords": None,
+                "program": "psi4",
+            },
+            "initial_molecule": self.qc_mol,
+        }
         angles = self.m.find_angles()
-        job_ids = []
+        # perturb the value of each angle by -30, -20, -10, 5, 10 ...
+        angle_bend_steps = [-30, -20, -10, 0, 10, 20, 30]
+        all_job_options = []
         for angle in angles:
-            # schema = {}
-            # perturb the value of each angle by -30, -20, -10, 5, 10 ...
-            # job_id = self.client.add_service('grid_optimization', schema)
-            # job_ids.append(job_id)
-            pass
-        return job_ids
+            job_option = copy.deepcopy(grid_opt_option_template)
+            job_option['keywords']['scans'][0]['indices'] = list(angle)
+            job_option['keywords']['scans'][0]['steps'] = angle_bend_steps
+            all_job_options.append(job_option)
+        r = self.client.add_service(all_job_options)
+        return r.ids
 
     def submit_vib_hessian_jobs(self):
         #schema = {}

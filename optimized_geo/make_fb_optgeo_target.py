a53b46ea3cc623cc53104b13f7daedeb5c0402b5
qyd <saviorbruce@gmail.com>
2019-07-03 09:12:53 -0700

    improve query performance and fix bugs

--- optimized_geo/make_fb_optgeo_target.py
+++ optimized_geo/make_fb_optgeo_target.py
@@ -5,6 +5,8 @@ import cmiles
 from openeye import oechem
 import qcportal as ptl
 client = ptl.FractalClient('https://api.qcarchive.molssi.org:443/')
+from openforcefield.topology import Molecule
+from forcebalance.molecule import Molecule
 
 ofs = oechem.oemolostream()
 
@@ -14,7 +16,8 @@ bond_denom 0.01
 angle_denom 0.03
 dihedral_denom 0.2
 improper_denom 0.2
-$end"""
+$end
+"""
 
 target_template = """
 $system
@@ -38,21 +41,28 @@ def load_final_molecules(dataset_name):
     # load dataset from public qcfractal server
     ds = client.get_collection("OptimizationDataset", dataset_name)
     spec_name = ds.list_specifications().index[0]
-    print(f"Loading final molecules from {dataset_name} spec [{spec_name}]")
+    print(f"Loading final molecules from [ {dataset_name} ] spec [{spec_name}]")
     print(f"Found {len(ds.df)} data entries")
     # load optimization record ids from the dataset
     map_optRecordId_mIndex = {}
     for m_index in ds.df.index:
-        data_entry = ds.get_entry(index)
+        data_entry = ds.get_entry(m_index)
         optRecordId = data_entry.object_map[spec_name]
         map_optRecordId_mIndex[optRecordId] = m_index
     print(f"Found {len(map_optRecordId_mIndex)} optimization records")
     # query all opt records at the same time
     optRecord_ids = list(map_optRecordId_mIndex.keys())
-    final_molecules = {}
+    final_molecules_ids_map = {}
     for optRecord in client.query_procedures(id=optRecord_ids):
         m_index = map_optRecordId_mIndex[optRecord.id]
-        final_molecules[m_index] = optRecord.get_final_molecule()
+        if optRecord.final_molecule:
+            final_molecules_ids_map[optRecord.final_molecule] = m_index
+    # query all final molecules
+    final_molecules = {}
+    final_molecule_ids = list(final_molecules_ids_map.keys())
+    for molecule in client.query_molecules(final_molecule_ids):
+        m_index = final_molecules_ids_map[molecule.id]
+        final_molecules[m_index] = molecule
     print(f"Loaded {len(final_molecules)} final molecules")
     return final_molecules
 
@@ -67,13 +77,18 @@ def get_int_fmt_string(n):
 def write_molecule_files(molecule, name):
     qcjson_mol = molecule.json_dict()
     oemol = cmiles.utils.load_molecule(qcjson_mol)
-    # write xyz, mol2 and pdb file
-    for ext in ['xyz', 'mol2', 'pdb']:
+    # write the mol2 file and xyz file
+    for ext in ['xyz', 'mol2']:
         ofs.open(f'{name}.{ext}')
         oechem.OEWriteMolecule(ofs, oemol)
         ofs.close()
+    # use ForceBalance Molecule to generate pdb file
+    # because the oechem.OEWriteMolecule will mess up with atom indices
+    fbmol = Molecule(f'{name}.mol2')
+    fbmol.write(f'{name}.pdb')
 
-def make_fb_targets(dataset_name):
+
+def make_optgeo_target(dataset_name):
     final_molecules = load_final_molecules(dataset_name)
     # get into targets folder
     if not os.path.exists('targets'):
@@ -81,7 +96,7 @@ def make_fb_targets(dataset_name):
     os.chdir('targets')
     # prepare output optget target folder
     target_name = 'optgeo_' + dataset_name.replace(' ', '_')
-    target_folder = os.path.join(out_folder, target_name)
+    target_folder = os.path.join(target_name)
     if not os.path.exists(target_folder):
         os.mkdir(target_folder)
     os.chdir(target_folder)
@@ -89,7 +104,7 @@ def make_fb_targets(dataset_name):
     # write notes
     fnotes = open('notes.txt', 'w')
     fnotes.write("Prepared by make_fb_optgeo_target.py\n")
-    fnotes.write(f"Data loaded from {dataset_name}\n"))
+    fnotes.write(f"Data loaded from {dataset_name}\n")
     with open("optgeo_options.txt", "w") as optfile:
         optfile.write(global_opts)
         target_index = 0
@@ -98,19 +113,24 @@ def make_fb_targets(dataset_name):
         for m_index, molecule in final_molecules.items():
             idx_str = idx_fmt_string.format(target_index)
             name = f"{idx_str}_{molecule.name}"
+            # if molecule.molecular_charge != 0:
+            #     fnotes.write(f'{name} : {m_index} skipped - charge {molecule.molecular_charge}\n')
+            #     continue
             # write molecule files
             write_molecule_files(molecule, name)
             # write $system block in optgeo_options.txt
-            optfile.write(target_template.format(name))
+            optfile.write(target_template.format(name=name))
             # write the original m_index
-            fnotes.write(f'{name} : {m_index}')
+            fnotes.write(f'{name} : {m_index}\n')
+            target_index += 1
     fnotes.close()
     os.chdir('..')
     # write a targets.in file
     with open('targets.in', 'w') as outfile:
-        outfile.write(target_opts.format(target_name))
+        outfile.write(target_opts.format(name=target_name))
+    os.chdir('..')
     print(f"Targets generation finished!")
-    print(f"You can copy contents in {os.path.join(out_folder, 'targets.in')} to your ForceBalance input file.")
+    print(f"You can copy contents in {os.path.join('targets', 'targets.in')} to your ForceBalance input file.")
 
 
 

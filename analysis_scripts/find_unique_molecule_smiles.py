3b48775a48e464b57810596283a997b258d77e0c
qyd <saviorbruce@gmail.com>
2019-09-25 11:27:13 -0700

    use canonical_isomeric_smiles in find_unique_molecule_smiles.py

--- analysis_scripts/find_unique_molecule_smiles.py
+++ analysis_scripts/find_unique_molecule_smiles.py
@@ -3,6 +3,7 @@ import json
 
 def find_unique_smiles(targets_folder):
     unique_smiles = {}
+    dataset_entries = {}
     for fol in os.listdir(targets_folder):
         p = os.path.join(targets_folder, fol)
         if not os.path.isdir(p): continue
@@ -13,6 +14,7 @@ def find_unique_smiles(targets_folder):
                     ls = line.split()
                     if len(ls) > 0 and ls[1] == ':':
                         label = ls[2]
+                        dataset_entries[label] = p
                         sm = label.rsplit('-',maxsplit=1)[0]
                         unique_smiles[sm] = p
         elif fol.startswith('vibfreq_'):
@@ -20,20 +22,23 @@ def find_unique_smiles(targets_folder):
             with open(os.path.join(p, 'note.txt')) as f:
                 for line in f:
                     label = line.rsplit(maxsplit=1)[-1]
+                    dataset_entries[label] = p
                     sm = label.rsplit('-',maxsplit=1)[0]
                     unique_smiles[sm] = p
         elif fol.startswith('td_'):
             # parse all smiles from each torsionprofile target
             with open(os.path.join(p, 'metadata.json')) as f:
                 d = json.load(f)
-                sm = d['canonical_smiles']
+                label = d["entry_label"]
+                dataset_entries[label] = p
+                sm = d['attributes']['canonical_isomeric_smiles']
                 unique_smiles[sm] = p
-    return unique_smiles
+    return unique_smiles, dataset_entries
 
 
-def print_each_line(unique_smiles):
+def print_each_line(unique_smiles, **kwargs):
     for s in sorted(unique_smiles):
-        print(f"{s:80} {unique_smiles[s]}")
+        print(f"{s:80} {unique_smiles[s]}", **kwargs)
 
 def main():
     import argparse
@@ -41,9 +46,12 @@ def main():
     parser.add_argument('-t', '--targets_folder', default='targets')
     args = parser.parse_args()
 
-    unique_smiles = find_unique_smiles(args.targets_folder)
+    unique_smiles, dataset_entries = find_unique_smiles(args.targets_folder)
 
     print_each_line(unique_smiles)
+
+    with open("dataset_entries.txt", "w") as f:
+        print_each_line(dataset_entries, file=f)
 
 if __name__ == "__main__":
     main()
